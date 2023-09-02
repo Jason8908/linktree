07041eeeaedd1b445e3adee5393072f203056f06
Jason8908 <jasonsu894@gmail.com>
2023-09-01 23:13:33 -0400

    LINK-3: Adding endpoint to perform user registration.

--- api/components/services.js
+++ api/components/services.js
@@ -1,4 +1,6 @@
 const { APIResponse } = require("../models/response");
+const config = require('../config.json');
+const bcrypt = require('bcryptjs');
 
 module.exports.APIService = class APIService {
     // Class fields and constructor
@@ -7,6 +9,24 @@ module.exports.APIService = class APIService {
         this.database = database;
     }
     // Methods
+    async registerUserMethod(username, password) {
+        // Check if user is already registered.
+        let user = await this.database.getUserByUsername(username);
+        if (user)
+            return new APIResponse(409, false, 'User is already registered.');
+        // Register the user.
+        let hash = this.#encrypt(password);
+        await this.database.registerUser(username, hash);
+        return new APIResponse(201, true, 'Successfully registered.');
+    }
+    // Helpers
+    #encrypt(data) {
+        return bcrypt.hashSync(data, config.hash.saltLength);
+    }
+    #checkHash(expected, hash) {
+        return bcrypt.compareSync(expected, hash);
+    }
+
     // Sample methods
     // async sampleWriteMethod(data) {
     //     let res;
